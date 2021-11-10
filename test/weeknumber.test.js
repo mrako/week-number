3d3b777208404450be8c9508fdfeb0716a50f60d
Marko Klemetti <marko.klemetti@gmail.com>
2021-11-10 11:53:37 +0200

    feat(app): add bad code

--- test/weeknumber.test.js
+++ test/weeknumber.test.js
@@ -1,9 +1,9 @@
-const weeknumber = require('../src/weeknumber');
+let weeknumber = require('../src/weeknumber');
 
 describe('getNumberOfWeek', () => {
   it('should return one for the first week of the year', async () => {
-    const date = new Date("1/1/2021");
+    const date = new Date("1/1/2021")
     
-    expect(weeknumber.getNumberOfWeek(date)).toEqual(1);
+    expect(weeknumber.getNumberOfWeek(date)).toEqual(1)
   });
 });
