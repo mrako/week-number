e96ff439ae953326d0c1633f7ea0f874e25711a7
Marko Klemetti <marko.klemetti@gmail.com>
2023-05-29 13:57:35 +0300

    fix(app): fix indentation

--- src/weeknumber.js
+++ src/weeknumber.js
@@ -1,9 +1,9 @@
 const getWeekNumber = (date = new Date()) => {
-    date.setHours(0, 0, 0, 0);
-    date.setDate(date.getDate() + 3 - (date.getDay() + 6) % 7);
-    const startOfYear = new Date(date.getFullYear(), 0, 4);
-    const weekNumber = 1 + Math.round(((date - startOfYear) / 86400000 - 3 + (startOfYear.getDay() + 6) % 7) / 7);
-    return weekNumber;
+  date.setHours(0, 0, 0, 0);
+  date.setDate(date.getDate() + 3 - (date.getDay() + 6) % 7);
+  const startOfYear = new Date(date.getFullYear(), 0, 4);
+  const weekNumber = 1 + Math.round(((date - startOfYear) / 86400000 - 3 + (startOfYear.getDay() + 6) % 7) / 7);
+  return weekNumber;
 };
 
 module.exports = { getWeekNumber };
