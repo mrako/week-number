fa6694f46456188ba975ec7972afb5727610f8a0
Marko Klemetti <marko.klemetti@gmail.com>
2023-06-06 14:05:11 +0300

    fix(app): update code to pass tests

--- src/weeknumber.js
+++ src/weeknumber.js
@@ -1,6 +1,9 @@
 function getWeekNumber(date = new Date()) {
-  const onejan = new Date(date.getFullYear(), 0, 1);
-  return Math.ceil(((date - onejan) / 86400000 + onejan.getDay() + 1) / 7);
+  date.setHours(0, 0, 0, 0);
+  date.setDate(date.getDate() + 3 - (date.getDay() + 6) % 7);
+  const startOfYear = new Date(date.getFullYear(), 0, 4);
+  const weekNumber = 1 + Math.round(((date - startOfYear) / 86400000 - 3 + (startOfYear.getDay() + 6) % 7) / 7);
+  return weekNumber;
 }
 
 module.exports = { getWeekNumber };
