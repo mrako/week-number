0f50b28ef08b2fab2c093403578f73db236ae83d
Marko Klemetti <marko.klemetti@gmail.com>
2023-06-08 11:48:30 +0200

    feat(app): update to using react

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import { createRoot } from 'react-dom';
+
+import { getWeekNumber } from './weeknumber';
+
+const App = () => (
+  <h1>{ getWeekNumber() }</h1>
+);
+
+const container = document.getElementById("app");
+const root = createRoot(container)
+root.render(<App />);
