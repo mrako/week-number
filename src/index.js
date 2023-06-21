08089113741645198a42138ba3811f0943703b21
Marko Klemetti <marko.klemetti@gmail.com>
2023-06-21 12:09:24 +0300

    feat(app): add collecting date as a param from url

--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { createRoot } from 'react-dom/client';
 
 import { getWeekNumber } from './weeknumber';
 
-const App = () => (
-  <h1>{ getWeekNumber ? getWeekNumber() : '-' }</h1>
-);
+const App = () => {
+  const [date, setDate] = useState();
+
+  useEffect(() => {
+    const searchParams = new URLSearchParams(window.location.search);
+    const date = Date.parse(searchParams.get('date'));
+
+    setDate(isNaN(date) ? new Date() : new Date(date));
+  }, []);
+
+  if (date) {
+    return <h1>{ getWeekNumber ? getWeekNumber(date) : '-' }</h1>;
+  }
+
+  return null;
+};
 
 const container = document.getElementById("app");
 const root = createRoot(container);
