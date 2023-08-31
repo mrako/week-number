df4d6a3fdf2cdb5b9a900075426fe2ca43a65d12
Marko Klemetti <marko.klemetti@gmail.com>
2023-08-31 15:11:32 +0300

    feat(app): update colors to blue

--- src/weeknumber.js
+++ src/weeknumber.js
@@ -1,1 +1,6 @@
+function getWeekNumber(date = new Date()) {
+  const onejan = new Date(date.getFullYear(), 0, 1)
+  return Math.ceil((((date - onejan) / 86400000) + onejan.getDay() + 1) / 7)
+}
 
+module.exports = {getWeekNumber};
