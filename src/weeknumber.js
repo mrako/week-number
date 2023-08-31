1196e94b9c9999bb2f460e4502a1ce713d8fc843
Marko Klemetti <marko.klemetti@gmail.com>
2023-08-31 14:18:58 +0300

    update colors to grey

--- src/weeknumber.js
+++ src/weeknumber.js
@@ -1,8 +1,1 @@
-function getWeekNumber(date = new Date()) {
-    const firstDayOfYear = new Date(date.getFullYear(), 0, 1);
-    const daysSinceFirstDayOfYear = (date - firstDayOfYear) / 86400000;
-    const weekNumber = Math.ceil((daysSinceFirstDayOfYear + firstDayOfYear.getDay() + 1) / 7);
-    return weekNumber;
-}
 
-module.exports = { getWeekNumber };
