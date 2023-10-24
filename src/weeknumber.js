387c2634340cc1e17a0dbc2525eaa8a643be21e2
Marko Klemetti <marko.klemetti@gmail.com>
2023-10-24 11:47:20 +0200

    feat: change colors to blue

--- src/weeknumber.js
+++ src/weeknumber.js
@@ -1,15 +1,8 @@
 function getWeekNumber(date = new Date()) {
-      // Get Thursday of the target week
-    const thursday = new Date(date);
-    thursday.setDate(thursday.getDate() + 3 - (thursday.getDay() + 6) % 7);
+    const firstDayOfYear = new Date(date.getFullYear(), 0, 1);
+    const pastDaysOfYear = (date - firstDayOfYear) / 86400000;
 
-    // Get the first Thursday of the year
-    const yearStart = new Date(thursday.getFullYear(), 0, 4);
+    return Math.ceil((pastDaysOfYear + firstDayOfYear.getDay() + 1) / 7);
 
-    // Calculate full weeks to the target Thursday
-    const weekNumber = 1 + Math.round(((thursday - yearStart) / (24 * 60 * 60 * 1000) - 3 + (yearStart.getDay() + 6) % 7) / 7);
-
-    return weekNumber;
 }
-
-module.exports = { getWeekNumber };
+module.exports = {getWeekNumber};
