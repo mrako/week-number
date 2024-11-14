b380299961d4383c087d7a8551efc61b8b22888b
Marko Klemetti <marko.klemetti@gmail.com>
2024-11-14 15:17:55 +0200

    feat: updated colors to blue

--- src/weeknumber.js
+++ src/weeknumber.js
@@ -1,8 +1,7 @@
 function getWeekNumber(date = new Date()) {
   const firstDayOfYear = new Date(date.getFullYear(), 0, 1);
   const pastDaysOfYear = (date - firstDayOfYear) / 86400000;
-
   return Math.ceil((pastDaysOfYear + firstDayOfYear.getDay() + 1) / 7);
 }
 
-module.exports = { getWeekNumber };
+module.exports = {getWeekNumber};
