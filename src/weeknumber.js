3fa39664eb062a5803a61f8193ae65587fe3c113
Marko Klemetti <marko.klemetti@gmail.com>
2025-02-26 09:46:08 +0200

    chore: revert week code

--- src/weeknumber.js
+++ src/weeknumber.js
@@ -1,18 +1,7 @@
-const weeknumber = {
-  getWeekNumber: function(date) {
-    if (!date) {
-      date = new Date();
-    }
-    const target = new Date(date.valueOf());
-    const dayNr = (date.getDay() + 6) % 7;
-    target.setDate(target.getDate() - dayNr + 3);
-    const firstThursday = target.valueOf();
-    target.setMonth(0, 1);
-    if (target.getDay() !== 4) {
-      target.setMonth(0, 1 + ((4 - target.getDay()) + 7) % 7);
-    }
-    return 1 + Math.ceil((firstThursday - target) / 604800000);
-  }
-};
+function getWeekNumber(date = new Date()) {
+  const firstDayOfYear = new Date(date.getFullYear(), 0, 1);
+  const pastDaysOfYear = (date - firstDayOfYear) / 86400000;
+  return Math.ceil((pastDaysOfYear + firstDayOfYear.getDay() + 1) / 7);
+}
 
-module.exports = weeknumber;
+module.exports = {getWeekNumber};
