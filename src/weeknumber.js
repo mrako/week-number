a38ad056ab94644d32e16a666a03168ed626d70a
Marko Klemetti <marko.klemetti@gmail.com>
2025-03-06 11:47:54 +0200

    feat: revert colors

--- src/weeknumber.js
+++ src/weeknumber.js
@@ -1,6 +1,8 @@
 function getWeekNumber(date = new Date()) {
-    const onejan = new Date(date.getFullYear(), 0, 1);
-    return Math.ceil((((date - onejan) / 86400000) + onejan.getDay() + 1) / 7);
+  const startOfYear = new Date(date.getFullYear(), 0, 1);
+  const diff = date - startOfYear;
+  const oneWeek = 1000 * 60 * 60 * 24 * 7;
+  return Math.floor(diff / oneWeek) + 1;
 }
 
 module.exports = {getWeekNumber};
